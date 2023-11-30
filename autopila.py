470ceb9a5bd76211cb6acd6311157c02674a9654
esquincaae <97558097+esquincaae@users.noreply.github.com>
2023-11-29 21:38:50 -0600

    ahora el automata acepta valores flotantes

--- autopila.py
+++ autopila.py
@@ -5,50 +5,61 @@ class AutomataDePila:
         self.current_state = 'q0'
         self.stack = ['Z']  # Pila con símbolo inicial Z
         self.transitions = {
-            # Transiciones y operaciones de pila
+            # Transiciones actualizadas con categorías
             ('q0', 'VR', 'Z'): ('q1', 'Z'),
             ('q1', 'T', 'Z'): ('q2', 'Z'),
-            ('q2', 'L', 'Z'): ('q3', 'Z'),
+            ('q2', 'LETTER', 'Z'): ('q3', 'Z'),
             ('q3', 'I', 'Z'): ('q4', 'Z'),
-            ('q4', 'N', 'Z'): ('q5', 'Z'), ('q4', 'VB', 'Z'): ('q5', 'Z'), ('q4', 'L', 'Z'): ('q5', 'Z'),
-            ('q5', 'PC', 'Z'): ('qf', 'Z'), ('q5', 'N', 'Z'): ('q6', 'Z'),
-            ('q6', 'P', 'Z'): ('q7', 'Z'),
-            ('q7', 'N', 'Z'): ('q5', 'Z')
+            ('q4', 'DIGIT', 'Z'): ('q5', 'Z'), 
+            ('q4', 'FLOAT', 'Z'): ('q5', 'Z'),
+            ('q4', 'VB', 'Z'): ('q5', 'Z'),
+            ('q5', 'PC', 'Z'): ('qf', 'Z'),
+            # Añadir más transiciones según sea necesario
         }
 
     def transition(self, symbol):
-    # Ajustar el símbolo para que coincida con la gramática
         top_stack = self.stack[-1]
-        if symbol == 'var':
-            symbol = 'VR'
-        elif symbol in ['ent', 'flot', 'booleano', 'cadena']:
-            symbol = 'T'
-        elif symbol.isalpha() and self.current_state in ['q2', 'q4', 'q5', 'q6', 'q7']:
-            symbol = 'L'
-        elif symbol.isdigit() and self.current_state in ['q4', 'q5', 'q7']:
-            symbol = 'N'
-        elif symbol == '=':
-            symbol = 'I'
-        elif symbol == ';':
-            symbol = 'PC'
-        elif symbol == '.' and self.current_state == 'q6':
-            symbol = 'P'
 
-    # Actualizar el estado actual y manejar la pila
-        if (self.current_state, symbol, top_stack) in self.transitions:
-            self.current_state, new_stack_symbol = self.transitions[(self.current_state, symbol, top_stack)]
-            if new_stack_symbol != top_stack:
-                self.stack.pop()
-                if new_stack_symbol != '':
-                    self.stack.append(new_stack_symbol)
+        def process_symbol(sym):
+            if (self.current_state, sym, top_stack) in self.transitions:
+                self.current_state, new_stack_symbol = self.transitions[(self.current_state, sym, top_stack)]
+                if new_stack_symbol != top_stack:
+                    self.stack.pop()
+                    if new_stack_symbol != '':
+                        self.stack.append(new_stack_symbol)
+
+        # Clasificación de símbolos
+        if symbol == 'var':
+            process_symbol('VR')
+        elif symbol in ['ent', 'flot', 'booleano', 'cadena']:
+            process_symbol('T')
+        elif symbol.isalpha() and symbol not in ['false', 'true']:
+            process_symbol('LETTER')
+        elif symbol in ['false', 'true']:
+            process_symbol('VB')
+        elif symbol == '=':
+            process_symbol('I')
+        elif symbol.isdigit():
+            process_symbol('DIGIT')
+        elif self.is_float(symbol):
+            process_symbol('FLOAT')
+        elif symbol == ';':
+            process_symbol('PC')
+        # Añadir más lógica según sea necesario
+
+    def is_float(self, string):
+        try:
+            float(string)
+            return True
+        except ValueError:
+            return False
 
     def reset(self):
         self.current_state = 'q0'
-        self.stack = ['Z']  # Restablecer la pila al estado inicial
-
+        self.stack = ['Z']
 
     def process(self, tokens):
-        self.reset()  # Restablecer el autómata antes de procesar una nueva cadena
+        self.reset()
         for token in tokens:
             if token.strip():  # Ignorar espacios en blanco
                 self.transition(token.strip())
@@ -60,7 +71,6 @@ def submit():
     tokens = entry.get().split(" ")
     result = automata.process(tokens)
     result_label.config(text="Cadena aceptada" if result else "Cadena rechazada")
-
 
 # Configuración de la interfaz gráfica
 root = tk.Tk()
