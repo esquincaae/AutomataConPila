4c468c64c53130cd7a57ebe721a054483b7634fc
esquincaae <97558097+esquincaae@users.noreply.github.com>
2023-12-03 05:04:03 -0600

    se cambio la logica con la que se hacia el programa

--- autopila.py
+++ autopila.py
@@ -1,160 +1,168 @@
 import tkinter as tk
 from tkinter import messagebox
 
-class AutomataDePila:
-    def __init__(self, tipo_entrada):
-        self.tipo_entrada = tipo_entrada if tipo_entrada else 'variable'
-        self.current_state = 'SV1' if self.tipo_entrada == 'variable' else 'SF1' 
-        self.stack = []
+class Automata:
+    def __init__(self):
         self.transitions = {
-            # Transiciones para variables
-            ('SV1', 'VR', 'A'): ('SV2', 'B'),
-            ('SV2', 'T', 'B'): ('SV3', 'C'),
-            ('SV3', 'LETTER', 'C'): ('SV4', 'D'),
-            ('SV4', 'I', 'D'): ('SV5', 'E'),
-            ('SV5', 'DIGIT', 'E'): ('SV6', 'F'),
-            ('SV6', 'PC', 'F'): ('SVF', 'G'),
-            ('SV6', 'P', 'F '): ('SV7', 'H'),
-            ('SV7', 'DIGIT', 'H'): ('SV8', 'I'),
-            ('SV8', 'PC', 'I'): ('SVF', 'J'),
-            # Transiciones para funciones
-            ('SF1', 'FN', 'A1'):('SF2', 'B1'),
-            ('SF2', 'LETTER', 'B1'):('SF3', 'C1'),
-            ('SF3', 'X1', 'C1'):('SF4', 'D1'),
-            ('SF4', 'X2', 'D1'):('SF5', 'E1'),
-            ('SF5', 'X3', 'E1'):('SF6', 'F1'),
-            ('SF6', 'X5', 'F1'):('SF7', 'G1'),
-            ('SF7', 'X4', 'G1'):('SFF', 'H1'),
-            # Transiciones para condicional
-            ('SC1', 'IF', 'A2'):('SC2', 'B2'),
-            ('SC2', 'X1', 'B2'):('SC3', 'C2'),
-            ('SC3', 'DIGIT', 'C2'):('SC4', 'D2'),
-            ('SC4', 'CN', 'D2'):('SC5', 'E2'),
-            ('SC5', 'DIGIT', 'E2'):('SC6', 'F2'),
-            ('SC6', 'X2', 'F2'):('SC7', 'G2'),
-            ('SC7', 'X3', 'G2'):('SC8', 'H2') or ('SF6', 'I2'),
-            ('SC8', 'X5', 'H2'):('SC9', 'I2'),
-            ('SC9', 'X4', 'I2'):('SC10', 'J2'),
-            ('SC10', 'EL', 'J2'):('SC11', 'K2'),
-            ('SC11', 'X3', 'K2'):('SCF', 'L2'),
-            #Transiciones para ciclo
-            ('SCI1', 'CY', 'A3'): ('SCI2', 'B3'),
-            ('SCI2', 'X1', 'B3'): ('SCI3', 'C3'),
-            ('SCI3', 'VR', 'C3'): ('SCI4', 'D3'),
-            ('SCI4', 'T', 'D3'): ('SCI5', 'E3'),
-            ('SCI4', 'L', 'E3'): ('SCI6', 'F3'),
-            ('SCI6', 'I', 'F3'): ('SCI7', 'G3'),
-            ('SCI7', 'N', 'G3'): ('SCI8', 'H3'),
-            ('SCI8', 'PC', 'H3'): ('SCI9', 'I3'),
-            ('SCI9', 'L', 'I3'): ('SCI10', 'J3'),
-            ('SCI10', 'CN', 'J3'): ('SCI11', 'K3'),
-            ('SCI11', 'N', 'K3'): ('SCI12', 'L3'),
-            ('SCI12', 'PC', 'L3'): ('SCI13', 'O3'),
-            ('SCI13', 'L', 'O3'): ('SCI14', 'P3'),
-            ('SCI14', 'AD', 'P3'): ('SCI15', 'Q3'),
-            ('SCI15', 'X2', 'Q3'): ('SCI16', 'R3'),
-            ('SCI16', 'X2', 'R3'): ('SCI17', 'S3'),
-            ('SCI17', 'X3', 'S3'): ('SF6', 'O3')
+    # Transiciones para variables
+    ('SV1', 'VR'): ('SV2', 'A'),
+    ('SV2', 'T'): ('SV3', 'A'),
+    ('SV3', 'L'): ('SV4', 'A'),
+    ('SV4', 'I'): ('SV5', 'A'),
+    ('SV5', 'N'): ('SV6', 'A'),
+    ('SV5', 'VB'): ('SV6', 'A'),
+    ('SV5', 'L'): ('SV6', 'A'),
+    ('SV6', 'P'): ('SV7', 'A'),
+    ('SV7', 'N'): ('SV8', 'A'),
+    ('SV6', 'PC'): ('SVF', 'A'),
+    ('SV8', 'PC'): ('SVF', 'A'),  # cierre
+    ('SV8', 'PC'): ('SV1', 'A'),  # agregar otra variable
+    ('SV8', 'PC'): ('SF1', 'B'),  # agregar funcion
+    ('SV8', 'PC'): ('SC1', 'A'),  # agregar condicional
+    ('SV8', 'PC'): ('SCI1', 'A'),  # agregar ciclo
+    # Transiciones para funciones
+    ('SF1', 'FN'): ('SF2', 'A'),
+    ('SF2', 'L'): ('SF3', 'A'),
+    ('SF3', 'X1'): ('SF4', 'A'),
+    ('SF4', 'X2'): ('SF5', 'A'),
+    ('SF5', 'X3'): ('SF6', 'A'),
+    ('SF6', 'X5'): ('SF7', 'A'),
+    ('SF7', 'X4'): ('SFF', 'A'),  # cierre
+    ('SF7', 'X4'): ('SF1', 'A'),  # agregar otra funcion
+    ('SF7', 'X4'): ('SV1', 'A'),  # agregar variable
+    ('SF7', 'X4'): ('SC1', 'A'),  # agregar condicional
+    ('SF7', 'X4'): ('SCI1', 'A'),  # agregar ciclo
+    # Transiciones para condicional
+    ('SC1', 'IF'): ('SC2', 'A'),
+    ('SC2', 'X1'): ('SC3', 'A'),
+    ('SC3', 'N'): ('SC4', 'A'),
+    ('SC4', 'CN'): ('SC5', 'A'),
+    ('SC5', 'N'): ('SC6', 'A'),
+    ('SC6', 'X2'): ('SC7', 'A'),
+    ('SC7', 'X3'): ('SF6', 'A'),  # camino para cerrar el si
+    ('SC7', 'X3'): ('SC8', 'A'),  # camino para cerrar el si y abrir el sino
+    ('SC8', 'X5'): ('SC9', 'A'),
+    ('SC9', 'X4'): ('SC10', 'A'),
+    ('SC10', 'EL'): ('SC11', 'A'),
+    ('SC11', 'X3'): ('SF6', 'A'),  # continua en SF6
+    # Transiciones para ciclo
+    ('SCI1', 'CY'): ('SCI2', 'A'),
+    ('SCI2', 'X1'): ('SCI3', 'A'),
+    ('SCI3', 'VR'): ('SCI4', 'A'),
+    ('SCI4', 'T'): ('SCI5', 'A'),
+    ('SCI4', 'L'): ('SCI6', 'A'),
+    ('SCI6', 'I'): ('SCI7', 'A'),
+    ('SCI7', 'N'): ('SCI8', 'A'),
+    ('SCI8', 'PC'): ('SCI9', 'A'),
+    ('SCI9', 'L'): ('SCI10', 'A'),
+    ('SCI10', 'CN'): ('SCI11', 'A'),
+    ('SCI11', 'N'): ('SCI12', 'A'),
+    ('SCI12', 'PC'): ('SCI13', 'A'),
+    ('SCI13', 'L'): ('SCI14', 'A'),
+    ('SCI14', 'AD'): ('SCI15', 'A'),
+    ('SCI15', 'X2'): ('SCI16', 'A'),
+    ('SCI16', 'X3'): ('SF6', 'A'),  # continua en SF6
+}
+
+        self.stack = []
+        self.stack_symbol = 'A'
+        self.current_state = 'SV1'
+
+    def next_state(self):
+        # Verifica si hay elementos en la pila
+        sym = self.stack_symbol
+        if not self.stack:
+            raise ValueError("La pila está vacía")
+        else:
+            dato = self.stack[-1]
+            if dato == 'VR':
+                self.current_state = 'SV1'
+            else:
+                raise ValueError("Cima de la pila invalida")
+        # Obtiene el input_type de la cima de la pila
+        while self.stack:
+            print(f'Lyra: {self.stack}')
+            input_type = self.stack.pop()
+            # Clave de transición usando el estado actual y el input_type de la pila
+            transition_key = (self.current_state, input_type, sym)
+            transition = self.transitions.get(transition_key)
+            if transition is None:
+                raise ValueError(f"Transición no encontrada para: {transition_key}")
+            next_state, action = transition
+            self.current_state = next_state
+            if 
+        print(f'Lyra: {self.stack}')
+        return next_state, action
+
+    def procesar_entrada(self, entrada):
+        # Definir los tipos de tokens
+        tokens = {
+            'VR': ["var"],
+            'T': ["ent", "flot", "booleano", "cadena"],
+            'IF': ["si"],
+            'EL': ["sino"],
+            'CY': ["para"],
+            'FN': ["funcion"],
+            'X5': ["contenido"],
+            'VB': ["false", "true"],
+            'L': list("abcdefghijklmnñopqrstuvwxyz"),
+            'N': list("0123456789"),
+            'I': ["="],
+            'PC': [";"],
+            'P': ["."],
+            'CN': ["<", ">", "<=", ">=", "==", "!="],
+            'AD': ["++", "--"],
+            'X1': ["("],
+            'X2': [")"],
+            'X3': ["{"],
+            'X4': ["}"],
         }
 
-    def transition(self, symbol, stack_label):
-        top_stack = self.stack[-1] if self.stack else None
-        #print(f"Estado actual: {self.current_state}, Símbolo: {symbol}, Cima de la pila: {top_stack}")
-        
-        def process_symbol(sym):
-            if (self.current_state, sym, top_stack) in self.transitions:
-                self.current_state, new_stack_symbol = self.transitions[(self.current_state, sym, top_stack)]
-                self.stack.pop()
-                if new_stack_symbol != '':
-                    self.stack.append(new_stack_symbol)
-            stack_label.config(text="Pila: " + str(self.stack))
-            #print(f"Nuevo estado: {self.current_state}, Lyra: {self.stack}")
-        
-        # Clasificación de símbolos
-        if symbol == 'var':
-            process_symbol('VR')
-        elif symbol in ['ent', 'flot', 'booleano', 'cadena']:
-            process_symbol('T')
-        elif symbol.isalpha() and symbol not in ['false', 'true']:
-            process_symbol('LETTER')
-        elif symbol == '=':
-            process_symbol('I')
-        elif symbol.isdigit():
-            process_symbol('DIGIT')
-        elif symbol == ';':
-            process_symbol('PC')
-        elif symbol == '.':
-            process_symbol('P')
-        elif symbol == 'funcion':
-            process_symbol('FN')
-        elif symbol == '(':
-            process_symbol('X1')
-        elif symbol == ')':
-            process_symbol('X2')
-        elif symbol == '{':
-            process_symbol('X3')
-        elif symbol == 'contenido':
-            process_symbol('X5')    
-        elif symbol == '}':
-            process_symbol('X4')
-        elif symbol == 'si':
-            process_symbol('IF')
-        elif symbol == 'sino':
-            process_symbol('EL')
-        elif symbol == 'para':
-            process_symbol('CY')
-        elif symbol in ["<", ">", "<=", ">=", "==", "!="]:
-            process_symbol('CN')
-        elif symbol in ['++', '--']:
-            process_symbol('AD')
-        else:
-            pass
-        # Asegúrate de que la clasificación de símbolos esté completa
+        palabras = entrada.split()
+        palabras.reverse()  # Invierte el orden de las palabras
 
-    #def is_float(self, string):
-     #   try:
-      #      float(string)
-       #     return True
-        #except ValueError:
-         #   return False
+        for palabra in palabras:
+            simbolo_encontrado = False
+            for tipo, valores in tokens.items():
+                if palabra in valores or (tipo == 'L' and palabra.isalpha()) or (tipo == 'N' and palabra.isdigit()):
+                    self.stack.append(tipo)  # Guarda el tipo en la pila
+                    simbolo_encontrado = True
+                    break
+            if not simbolo_encontrado:
+                self.stack.append('DESCONOCIDO')
 
-    def process(self, stack_label):
-        while self.stack:
-            symbol = self.stack.pop()
-            print(f"Lyra{self.stack}")  # Imprimir el estado actual de la pila
-            self.transition(symbol, stack_label)
-            if self.current_state in ['SVF', 'SFF', 'SCF']:
-                return True
-        return False
+        if 'DESCONOCIDO' in self.stack:
+            raise ValueError(f"Símbolo desconocido en la entrada: {entrada}")
 
-def submit():
-    cadena = text_area.get("1.0", "end-1c")
-    tokens = cadena.split(" ")
-    automata.stack.extend(reversed(tokens))  # Agrega los tokens a la pila en orden inverso
-    #print(f"Lyra{automata.stack}")
+        return ' '.join(self.stack)
 
-    result = automata.process(stack_label)
-    result_label.config(text="Cadena aceptada" if result else "Cadena rechazada")
-    stack_label.config(text="Pila final: " + str(automata.stack))
+# Funciones para la interfaz gráfica
+def procesar():
+    entrada = text_area.get("1.0", tk.END).strip()
+    try:
+        simbolos_procesados = automata.procesar_entrada(entrada)
+        resultado.set(f"Símbolos procesados: {simbolos_procesados}")
+    except ValueError as e:
+        messagebox.showerror("Error", e)
+    automata.next_state()    
 
-root = tk.Tk()
-root.title("Lyra: Autómata de Pila")
+# Configuración de la ventana Tkinter
+ventana = tk.Tk()
+ventana.title("Autómata de Pila")
 
-automata = AutomataDePila(tipo_entrada='')
-
-entry_label = tk.Label(root, text="Ingrese la cadena a validar:")
-entry_label.pack()
-
-text_area = tk.Text(root, height=30, width=90)
+# Crea y configura widgets
+text_area = tk.Text(ventana, height=10, width=50)
 text_area.pack()
 
-submit_button = tk.Button(root, text="Procesar", command=submit)
-submit_button.pack()
+boton_procesar = tk.Button(ventana, text="Procesar", command=procesar)
+boton_procesar.pack()
 
-result_label = tk.Label(root, text="")
-result_label.pack()
+resultado = tk.StringVar()
+etiqueta_resultado = tk.Label(ventana, textvariable=resultado)
+etiqueta_resultado.pack()
 
-stack_label = tk.Label(root, text="Pila: " + str(automata.stack))
-stack_label.pack()
+# Crea una instancia del autómata
+automata = Automata()
 
-root.mainloop()
+# Ejecuta el bucle principal de la GUI
+ventana.mainloop()
